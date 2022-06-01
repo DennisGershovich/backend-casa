ab63617562bb480a0dad97c18e5b233ed6dc5b5b
Denis_Gershovich <denis.gershovich@gmail.com>
2022-06-01 17:24:27 +0300

    updated order api

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
 const stayRoutes = require('./api/stay/stay.routes')
-const orderRoutes = require('./api/stay/order.routes')
+const orderRoutes = require('./api/order/order.routes')
 
 const {setupSocketAPI} = require('./services/socket.service')
 
